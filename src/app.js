e96a9b36245ab6fcef77db91334976bc4c2c1c8e
Carlos Ardon <ardon7528@gmail.com>
2022-02-16 00:16:23 -0600

    creacion de ruta para el modulo de vehiculos desde la app

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ app.set('json spaces', 2);
 app.use(passport.initialize());
 //Declaramos las rutas que vamos a usar
 app.use('/uber/', require('./Rutas/index'));
+app.use('/uber/vehiculos', require('./Rutas/rutasVehiculos'));
 
 
 //Iniciaríamos el servidor
