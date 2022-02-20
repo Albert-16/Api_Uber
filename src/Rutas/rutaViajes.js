e77b962d1457eed271aba1971057a77deb240cc1
Angel Quintanilla <angeldavidquintanilla@gmail.com>
2022-02-19 21:47:11 -0600

    Validación ruta viajes

--- src/Rutas/rutaViajes.js
+++ src/Rutas/rutaViajes.js
@@ -1,6 +1,17 @@
 const { Router } = require('express');
 const controladorViaje = require('../controladores/ControladorViajes');
+const { body, query } = require('express-validator');
 const router = Router();
 router.get('/listar', controladorViaje.listar);
-router.post('/guardar', controladorViaje.guardar);
+router.post('/guardar', body('id_Viaje').isInt().withMessage('Debe enviar el ID de un viaje existente'),
+    body('latitud_Inicial').isString(100).withMessage('Es necesario ingresar una latitud inicial'),
+    body('longitud_Inicial').isString(100).withMessage('Es necesario ingresar una longitud inicial'),
+    body('latitud_Final').isString(100).withMessage('Es necesario ingresar una latitud final'),
+    body('longitud_Final').isString(100).withMessage('Es necesario ingresar una longitud final'),
+    body('fecha_Inicial').isDate.withMessage('Es necesario ingresar una fecha incial'),
+    body('fecha_Final').isDate.withMessage('Es necesario ingresar una fecha final'),
+    body('fecha').isDate.withMessage('Es necesario ingresar una fecha'),
+    body('direccion_Inicial').isString(200).withMessage('Es necesario ingresar una dirección inicial'),
+    body('direccion_Final').isString(200).withMessage('Es necesario ingresar una dirección final'),
+    body('total').isDecimal(10,2).withMessage('Es necesario ingresar un total valido'),controladorViaje.guardar);
 module.exports = router;
