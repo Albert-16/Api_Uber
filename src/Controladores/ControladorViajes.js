925b4da95440d7d9d884874b09defe0a6fb51383
naye-19 <78245529+naye-19@users.noreply.github.com>
2022-02-21 14:43:21 -0600

    Cambio en los mensajes de confirmación, error y advertencia.

--- src/Controladores/ControladorViajes.js
+++ src/Controladores/ControladorViajes.js
@@ -1,5 +1,6 @@
 //Cargamos el modelo que creamos
 const ModeloViaje = require('../modelos/modeloViajes');
+const msj = require("../Componentes/mensaje");
 
 //Definición de la función
 exports.listar = async (req, res)=>{ 
@@ -10,10 +11,10 @@ exports.listar = async (req, res)=>{
     //Validación por si los campos se encuentran vacios.
     if(listaViajes.length == 0) //Si lista viajes está vacío o nulo.
     {
-        res.send("No existen viajes en la base de datos.");
+        msj("Listar los viajes", "No existen viajes en la base de datos", 200, [], res);
     }
     else{ //Si lista viajes tiene datos.
-        res.json(listaViajes); 
+        msj("Éxito","Lista de viajes", 200, listaViajes, res); 
     }
 };
 
@@ -21,10 +22,10 @@ exports.listar = async (req, res)=>{
 exports.guardar = async (req, res)=>{ 
    
     console.log(req.body);
-    const { latitud_Inicial, longitud_Inicial, longitud_Final, latitud_Final, fecha_Inicial, fecha_Final,  fecha, direccion_Inicial, direccion_Final, total, distancia_Km} = req.body; 
+    const { latitud_Inicial, longitud_Inicial, longitud_Final, latitud_Final, fecha_Inicial, fecha_Final,  fecha, direccion_Inicial, direccion_Final, total} = req.body; 
     if(!latitud_Inicial || !longitud_Inicial || !longitud_Final || !latitud_Final || !fecha_Inicial || !fecha_Final || !fecha 
-        || !direccion_Inicial || !direccion_Final || !total || !distancia_Km){
-        res.send("Debe llenar los campos que son obligatorios");
+        || !direccion_Inicial || !direccion_Final || !total){
+        msj("Advertencia","Debe llenar los campos que son obligatorios", 200, [], res);
     }
     else{
         await ModeloViaje.create({
@@ -37,16 +38,15 @@ exports.guardar = async (req, res)=>{
             fecha: fecha,
             direccion_Inicial: direccion_Inicial,
             direccion_Final: direccion_Final,
-            total: total,
-            distancia_Km: distancia_Km
+            total: total
         })
         .then((data)=>{ 
             console.log(data);
-            res.send("El registro se almacenó correctamente.");
+            msj("Éxito", "El registro se almacenó correctamente.", 200, [], res);
         })
         .catch((error)=>{
             console.log(error);
-            res.send("Error al momento de guardar los datos");
+            msj("Error", "Error al momento de guardar los datos.", 200, [], res);
         });
     }
 };
