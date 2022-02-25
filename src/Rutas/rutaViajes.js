2e05baf53e62f274986f9cdc8460d494b713190c
naye-19 <78245529+naye-19@users.noreply.github.com>
2022-02-25 00:23:40 -0600

    Modificación de la ruta de viajes.

--- src/Rutas/rutaViajes.js
+++ src/Rutas/rutaViajes.js
@@ -13,4 +13,6 @@ router.post('/guardar', body('id_Viaje').isInt().withMessage('Debe enviar el ID 
     body('direccion_Inicial').isString(200).withMessage('Es necesario ingresar una dirección inicial'),
     body('direccion_Final').isString(200).withMessage('Es necesario ingresar una dirección final'),
     body('total').isDecimal().withMessage('Es necesario ingresar un total valido'),controladorViaje.guardar);
+router.put('/modificar', controladorViaje.modificar);
+router.put('/eliminar', controladorViaje.eliminar);
 module.exports = router;
