3b82893be1e8ab762bd1e77b9a72cd1547e6a578
naye-19 <78245529+naye-19@users.noreply.github.com>
2022-02-27 16:17:02 -0600

    Creación del controlador de ciudades con sus respectivas validaciones y funciones (listar, guardar, modificar y eliminar).

---
+++ src/Controladores/ControladorCiudades.js
@@ -0,0 +1,122 @@
+//Cargamos el modelo que creamos
+const ModeloCiudad = require('../Modelos/ModeloCiudad');
+const msj = require('../Componentes/mensaje');
+
+//Definicion de la función
+// L I S T A R -- C I U D A D E S 
+exports.listar = async (req, res) => {
+    try {
+        const listaCiudades = await ModeloCiudad.findAll();
+
+        //Validación por si los campos se encuentran vacios.
+        if (listaCiudades.length == 0) {
+            msj("Advertencia", "No existen ciudades en la base de datos.", 200, [], res);
+        }
+        else {
+            msj("Éxito", "Lista de Ciudades", 200, listaCiudades, res);
+        }
+    }
+    catch (error) {
+        res.status(500).json({
+            error: error.toString()
+        });
+    }
+};
+
+// G U A R D A R -- C I U D A D E S 
+exports.guardar = async (req, res) => {
+    try {
+        console.log(req.body);
+        const { descripcion_Ciudad } = req.body;
+        if (!descripcion_Ciudad) {
+            msj("Advertencia", "Debe llenar los campos que son obligatorios", 200, [], res);
+        }
+        else {
+            await ModeloCiudad.create({
+                descripcion_Ciudad: descripcion_Ciudad,
+            })
+                .then((data) => {
+                    msj("Éxito", "El registro se almacenó correctamente.", 200, data, res);
+                })
+                .catch((error) => {
+                    msj("Error", "Error al momento de guardar los datos", 200, error, res);
+                });
+
+        }
+    }
+    catch (error) {
+        res.status(500).json({
+            error: error.toString()
+        });
+    }
+};
+
+// M O D I F I C A R -- C I U D A D E S 
+exports.modificar = async (req, res) => {
+    try {
+        const { id_Ciudad } = req.query;
+        const { descripcion_Ciudad } = req.body;
+        if (!id_Ciudad || !descripcion_Ciudad) {
+            msj("Advertencia", "Debe enviar los datos completos", 200, [], res);
+        }
+        else {
+            var buscarCiudad = await ModeloCiudad.findOne({
+                where: {
+                    id_Ciudad: id_Ciudad
+                }
+            });
+            if (!buscarCiudad) {
+                msj("Advertencia", "El id de la ciudad no existe.", 200, [], res);
+            }
+            else {
+                buscarCiudad.descripcion_Ciudad = descripcion_Ciudad;
+                await buscarCiudad.save()
+                    .then((data) => {
+                        msj("Éxito", "El registro ha sido actualizado", 200, data, res);
+                    })
+                    .catch((error) => {
+                        msj("Error", "Error al momento de modificar el registro", 200, error, res);
+                    });
+            }
+        }
+    }
+    catch (error) {
+        res.status(500).json({
+            error: error.toString()
+        });
+    }
+};
+
+// E L I M I N A R -- C I U D A D E S 
+exports.eliminar = async (req, res) => {
+    try {
+        const { id_Ciudad } = req.query; //Siempre le enviamos el ID.
+        if (!id_Ciudad) {
+            msj("Advertencia", "Envíe los datos completos", 200, [], res);
+        }
+        else {
+            await ModeloCiudad.destroy({
+                where: {
+                    id_Ciudad: id_Ciudad
+                }
+            })
+                .then((data) => {
+                    console.log(data);
+                    if (data == 0) {
+                        msj("Advertencia", "El id de la ciudad no existe.", 200, [], res);
+                    }
+                    else {
+                        msj("Éxito", "El registro ha sido eliminado", 200, data, res);
+                    }
+                })
+                .catch((error) => {
+                    msj("Error", "Error al momento de eliminar el registro", 200, error, res);
+                });
+        }
+    }
+    catch (error) {
+        res.status(500).json({
+            error: error.toString()
+        });
+    }
+};
