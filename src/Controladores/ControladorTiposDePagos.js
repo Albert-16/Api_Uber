c52168c27479b172fe24df30509fb31e04bd9142
Luis Flores <lf016158@gmail.com>
2022-03-04 10:08:42 -0600

    se arreglo mensajes y el metodo listar y la autenticacion a las rutas

--- src/Controladores/ControladorTiposDePagos.js
+++ src/Controladores/ControladorTiposDePagos.js
@@ -1,85 +1,124 @@
 const ModeloTiposDePagos = require('../Modelos/ModeloTiposPagos');
-
+const msj = require("../Componentes/mensaje");
 const { validationResult } = require('express-validator');
 
 exports.inicio = async (req, res) => {
-    res.send("Estás en el Incio de Vehiculos_Ciudad");
+    msj("Inicio", "Estás en el Inicio de Tipos de Pago", 200, [], res);
 };
 
 exports.ListarRegistro = async (req, res) => {
-    const listaDeVehiculos = await ModeloTiposDePagos.findAll();
-    if (!listaDeVehiculos) {
-        res.send("No Registros vehiculos en la base de datos");
+
+    try {
+        const listaDeVehiculos = await ModeloTiposDePagos.findAll({
+            attributes: [
+                ['id_Tipos_Pago', '#ID'],
+                ['descripcion_Pago', 'Tipo de Pago']
+            ]
+        });
+        if (!listaDeVehiculos) {
+            msj("Lista Vaciá", "No Registros vehículos en la base de datos", 200, [], res);
+        }
+        else {
+            msj("Métodos de Pago", "Total de Registros: " + listaDeVehiculos.length, 200, listaDeVehiculos, res);
+        }
     }
-    else {
-        res.json(listaDeVehiculos);
+    catch (error) {
+        res.status(500).json({
+            error: error.toString()
+        });
     }
+
 };
 
 exports.GuardarRegistro = async (req, res) => {
-    const validacion = validationResult(req);
-    const { descripcion_Pago } = req.body;
-    if (!validacion.isEmpty()) {
-        
-        console.log(validacion.array());
-        res.send(validacion.array());
-    }
-    else {
-        if (!descripcion_Pago) {
-            res.send("[Advertencia] Debe enviar los datos completos [Advertencia]");
+    try {
+        const validacion = validationResult(req);
+        const { descripcion_Pago } = req.body;
+        if (!validacion.isEmpty()) {
+            msj("Datos Incorrectos", "Los datos ingresados no son validos", 200, validacion.array(), res);
         }
         else {
-            await ModeloTiposDePagos.create({
-                    descripcion_Pago
-            })
-                .then((data) => {
-                    res.send("Registro almacenado con éxito.");
-                })
-                .catch((error) => {
-                    res.send("¡Error al guardar los datos!");
-                    console.log(error);
+            if (!descripcion_Pago) {
+                msj("Advertencia", " Debe enviar los datos completos.", 200, [], res);
+            }
+            else {
+
+                const ExistTipoPago = await ModeloTiposDePagos.findOne({
+                    where: {
+                        descripcion_Pago: descripcion_Pago
+                    }
                 });
+
+                if (ExistTipoPago) {
+                    msj("Advertencia", "El tipo de pago que trata de ingresar ya existe.", 200, [], res);
+                }
+                else {
+                    await ModeloTiposDePagos.create({
+                        descripcion_Pago
+                    })
+                        .then((data) => {
+                            msj("Registro Guardado", "Registro almacenado con éxito.", 200, data, res);
+                        })
+                        .catch((error) => {
+                            msj("Error al Guardar", "¡Error al guardar los datos!", 200, [], res);
+
+                        });
+                }
+
+
+            }
         }
+    } catch (error) {
+        res.status(500).json({
+            error: error.toString()
+        });
     }
+
 };
 
 exports.ModificarRegistro = async (req, res) => {
-    const validacion = validationResult(req);
-    const { id_Tipos_Pago } = req.query;
-    const { descripcion_Pago } = req.body;
-    if(!validacion.isEmpty)
-    {
-        console.log(validacion.array());
-        res.send(validacion.array());
-    }
-    else{
-       
-        if (!id_Tipos_Pago  || !descripcion_Pago) {
-            res.send("[Advertencia] Debe enviar los datos completos [Advertencia]");
+    try {
+        const validacion = validationResult(req);
+        const { id_Tipos_Pago } = req.query;
+        const { descripcion_Pago } = req.body;
+        if (!validacion.isEmpty) {
+            msj("Datos Incorrectos", "Los datos ingresados no son validos", 200, validacion.array(), res);
         }
-        else{
-            var buscarVehiculo = await ModeloTiposDePagos.findOne({
-                where: {
-                    id_Tipos_Pago  : id_Tipos_Pago 
-                }
-            });
-            if (!buscarVehiculo) {
-                res.send("El id no existe o no se encuentra activo");
+        else {
+
+            if (!id_Tipos_Pago || !descripcion_Pago) {
+                msj("Advertencia", " Debe enviar los datos completos.", 200, [], res);
             }
             else {
-                buscarVehiculo.descripcion_Pago = descripcion_Pago;
+                var buscarTipoPago = await ModeloTiposDePagos.findOne({
+                    where: {
+                        id_Tipos_Pago: id_Tipos_Pago
+                    }
+                });
+                if (!buscarTipoPago) {
+                    msj("Tipo de pago no encontrado", "El id no existe o no se encuentra activo");
+                }
+                else {
+                    buscarTipoPago.descripcion_Pago = descripcion_Pago;
 
-                await buscarVehiculo.save()
-                    .then((data) => {
-                        console.log(data);
-                    })
-                    .catch((error) => {
-                        console.log(error);
-                        res.send("[Advertencia] Error al modificar el resgistro [Advertencia]");
-                    });
-               
+                    await buscarTipoPago.save()
+                        .then((data) => {
+                            msj("Registro Actualizado", "¡Registro modificado con éxito!", 200, data, res);
+                        })
+                        .catch((error) => {
+                            console.log(error);
+                            msj("Advertencia", "Error al modificar el registro.", 200, [], res);
+                        });
+
+                }
+
             }
-            res.send("¡Registro modificado con éxito!");
         }
+    } catch (error) {
+        res.status(500).json({
+            error: error.toString()
+        });
     }
+
 };
+
--- src/Rutas/rutasTiposDePagos.js
+++ src/Rutas/rutasTiposDePagos.js
@@ -3,19 +3,20 @@ const { Router } = require('express');
 //Cargamos el controlador Inicio
 const controladorTiposDePagos = require('../Controladores/ControladorTiposDePagos');
 const { body, query } = require('express-validator');
+const controladorSesiones = require('../Controladores/controladorSesiones');
 //Creamos objeto y lo instanciamos
 const router = Router();
 
-router.get('/',controladorTiposDePagos.inicio);
-router.get('/listar',controladorTiposDePagos.ListarRegistro);
+router.get('/',controladorSesiones.validarAutenticado,controladorTiposDePagos.inicio);
+router.get('/listar',controladorSesiones.validarAutenticado,controladorTiposDePagos.ListarRegistro);
 
-router.post('/guardar',
-body('descripcion_Pago').isLength({min:7}).withMessage("La placa debe contener un mínimo de 7 caracteres"),
+router.post('/guardar',controladorSesiones.validarAutenticado,
+body('descripcion_Pago').isLength({min:7}).withMessage("Debe contener un mínimo de 7 caracteres"),
 controladorTiposDePagos.GuardarRegistro);
 
-router.put('/modificar',
+router.put('/modificar',controladorSesiones.validarAutenticado,
 query(' id_Tipos_Pago ').isInt().withMessage("Debe enviar un ID válido"),
-body('descripcion_Pago').isLength({min:7}).withMessage("La placa debe contener un mínimo de 7 caracteres"),
+body('descripcion_Pago').isLength({min:7}).withMessage("Debe contener un mínimo de 7 caracteres"),
 controladorTiposDePagos.ModificarRegistro);
 
 module.exports = router;
