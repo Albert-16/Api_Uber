8ebd1750a5d4392f21dc591b12c544c19fdc1a1b
Luis Flores <lf016158@gmail.com>
2022-03-04 10:12:33 -0600

    se arreglo mensajes ,se arreglo validaciones y autenticacion en las rutas

--- src/Controladores/ControladorVehiculos.js
+++ src/Controladores/ControladorVehiculos.js
@@ -1,127 +1,160 @@
 const ModeloVehiculo = require('../Modelos/ModeloVehiculos');
-
+const msj = require('../Componentes/mensaje');
+const con = require('../Configuracion/coneccionMysql');
 const { validationResult } = require('express-validator');
 
 exports.inicio = async (req, res) => {
-    res.send("Estás en el Incio de Vehiculos");
+    msj("Inicio", "Estás en el Inicio de Vehículos", 200, [], res);
 };
 
 exports.ListarVehiculos = async (req, res) => {
-    const listaDeVehiculos = await ModeloVehiculo.findAll();
-    if (!listaDeVehiculos) {
-        res.send("No existen vehiculos en la base de datos");
-    }
-    else {
-        res.json(listaDeVehiculos);
+
+    try {
+        var ListaVehiculos = [];
+        const query = "select * from listarvehiculos;";
+        //Funcion para ejecutar un proceso almacenado
+        con.connect(function (err) {
+            if (err) throw err;
+            con.query(query, function (err, result, fields) {
+                if (err) throw err;
+                ListaVehiculos = result;
+                const totalRegistros = result.length;
+                if (!ListaVehiculos) {
+                    msj("Lista Vaciá", "No existen vehículos en la base de datos", 200, [], res);
+                }
+                else {
+                    msj("Lista de Vehículos", "Total de registros: " + totalRegistros, 200, ListaVehiculos, res);
+                }
+            });
+        });
+    } catch (error) {
+        res.status(500).json({
+            error: error.toString()
+        });
     }
 };
 
 exports.GuardarVehiculos = async (req, res) => {
-    const validacion = validationResult(req);
-    if (!validacion.isEmpty()) {
-        console.log(validacion.array());
-        res.send(validacion.array());
-    }
-    else {
-        const { placa, id_Modelo, anio, color } = req.body;
-
-        if (!placa || !id_Modelo || !anio || !color) {
-            res.send("[Advertencia] Debe enviar los datos completos [Advertencia]");
+    try {
+        const validacion = validationResult(req);
+        if (!validacion.isEmpty()) {
+            msj("Datos Incorrectos", "Los datos ingresados no son validos", 200, validacion.array(), res);
         }
         else {
-            await ModeloVehiculo.create({
-               placa, id_Modelo, anio, color
-            })
-                .then((data) => {
-                    res.send("Registro almacenado con éxito.");
+            const { placa, id_Modelo, anio, color } = req.body;
+
+            if (!placa || !id_Modelo || !anio || !color) {
+                msj("Advertencia", " Debe enviar los datos completos.", 200, [], res);
+            }
+            else {
+                await ModeloVehiculo.create({
+                    placa, id_Modelo, anio, color
                 })
-                .catch((error) => {
-                    res.send("¡Error al guardar los datos!");
-                    console.log(error);
-                });
+                    .then((data) => {
+                        msj("Registro Guardado", "Registro almacenado con éxito.", 200, data, res);
+                    })
+                    .catch((error) => {
+                        msj("Error", "¡Error al guardar los datos!", 200, [], res);
+                        console.log(error);
+                    });
+            }
         }
+    } catch (error) {
+        res.status(500).json({
+            error: error.toString()
+        });
     }
+
 };
 
 exports.ModificarVehiculos = async (req, res) => {
-    const validacion = validationResult(req);
-    const { id_Vehiculo } = req.query;
-    const { placa, id_Modelo, anio, color } = req.body;
-    if(!validacion.isEmpty)
-    {
-        console.log(validacion.array());
-        res.send(validacion.array());
-    }
-    else{
-        if (!id_Vehiculo || !placa || !id_Modelo || !anio || !color) {
-            res.send("[Advertencia] Debe enviar los datos completos [Advertencia]");
+    try {
+        const validacion = validationResult(req);
+        const { id_Vehiculo } = req.query;
+        const { placa, id_Modelo, anio, color } = req.body;
+        if (!validacion.isEmpty()) {
+            msj("Datos Incorrectos", "Los datos ingresados no son validos", 200, validacion.array(), res);
         }
-        else{
-            var buscarVehiculo = await ModeloVehiculo.findOne({
-                where: {
-                    id_Vehiculo : id_Vehiculo
-                }
-            });
-            if (!buscarVehiculo) {
-                res.send("El id no existe o no se encuentra activo");
+        else {
+            if (!id_Vehiculo || !placa || !id_Modelo || !anio || !color) {
+                msj("Advertencia", " Debe enviar los datos completos.", 200, [], res);
             }
             else {
-                buscarVehiculo.placa =placa;
-                buscarVehiculo.id_Modelo =id_Modelo;
-                buscarVehiculo.anio =anio;
-                buscarVehiculo.color =color;
-                await buscarVehiculo.save()
-                    .then((data) => {
-                        console.log(data);
-                    })
-                    .catch((error) => {
-                        console.log(error);
-                        res.send("[Advertencia] Error al modificar el resgistro [Advertencia]");
-                    });
-               
+                var buscarVehiculo = await ModeloVehiculo.findOne({
+                    where: {
+                        id_Vehiculo: id_Vehiculo
+                    }
+                });
+                if (!buscarVehiculo) {
+                    msj("Vehiculo no encontrado", "No se encontró el vehículo.", 200, [], res);
+                }
+                else {
+                    buscarVehiculo.placa = placa;
+                    buscarVehiculo.id_Modelo = id_Modelo;
+                    buscarVehiculo.anio = anio;
+                    buscarVehiculo.color = color;
+                    await buscarVehiculo.save()
+                        .then((data) => {
+                            msj("Registro Actualizado", "¡Vehiculo modificado con éxito!", 200, data, res);
+                        })
+                        .catch((error) => {
+                            console.log(error);
+                            msj("Advertencia", "Error al modificar el registro [Advertencia]", 200, error, res);
+                        });
+
+                }
+
             }
-            res.send("¡Vehiculo modificado con éxito!");
         }
+    } catch (error) {
+        res.status(500).json({
+            error: error.toString()
+        });
     }
+
 };
 
 exports.EliminarVehiculos = async (req, res) => {
-    const validacion = validationResult(req);
-    const { id_Vehiculo } = req.query;
-    const { estado } = req.body;
-    if(!validacion.isEmpty)
-    {
-        console.log(validacion.array());
-        res.send(validacion.array());
-    }
-    else{
-       
-        if (!id_Vehiculo) {
-            console.log(id_Vehiculo);
-            console.log(estado);
-            res.send("[Advertencia] Debe enviar los datos completos [Advertencia]");
+
+    try {
+        const validacion = validationResult(req);
+        const { id_Vehiculo } = req.query;
+        const { estado } = req.body;
+        if (!validacion.isEmpty()) {
+            msj("Datos Incorrectos", "Los datos ingresados no son validos", 200, validacion.array(), res);
         }
-        else{
-            var buscarVehiculo = await ModeloVehiculo.findOne({
-                where: {
-                    id_Vehiculo: id_Vehiculo
-                }
-            });
-            if (!buscarVehiculo) {
-                res.send("El id no existe o no se encuentra activo");
+        else {
+
+            if (!id_Vehiculo) {
+                msj("Advertencia", " Debe enviar los datos completos.", 200, [], res);
             }
             else {
-                buscarVehiculo.estado = estado;
-                await buscarVehiculo.save()
-                    .then((data) => {
-                        console.log(data);
-                    })
-                    .catch((error) => {
-                        console.log(error);
-                        res.send("[Advertencia] Error al modificar el resgistro [Advertencia]");
-                    });
+                var buscarVehiculo = await ModeloVehiculo.findOne({
+                    where: {
+                        id_Vehiculo: id_Vehiculo
+                    }
+                });
+                if (!buscarVehiculo) {
+                    msj("Vehiculo no encontrado", "No se encontró el vehículo.", 200, [], res);
+                }
+                else {
+                    buscarVehiculo.estado = estado;
+                    await buscarVehiculo.save()
+                        .then((data) => {
+                            msj("Registro Eliminado", "¡Vehiculo Eliminado con éxito!", 200, data, res);
+                        })
+                        .catch((error) => {
+                            console.log(error);
+                            msj("Advertencia", " Error al eliminar el registro", 200, error, res);
+                        });
+                }
+
             }
-            res.send("¡Vehiculo modificado con éxito!");
         }
+    } catch (error) {
+        res.status(500).json({
+            error: error.toString()
+        });
     }
+
 };
--- src/Rutas/rutasVehiculos.js
+++ src/Rutas/rutasVehiculos.js
@@ -2,29 +2,30 @@
 const { Router } = require('express');
 //Cargamos el controlador Inicio
 const controladorVehiculo = require('../Controladores/ControladorVehiculos');
+const controladorSesiones = require('../Controladores/controladorSesiones');
 const { body, query } = require('express-validator');
 //Creamos objeto y lo instanciamos
 const router = Router();
 
-router.get('/',controladorVehiculo.inicio);
-router.get('/listar',controladorVehiculo.ListarVehiculos);
+router.get('/',controladorVehiculo.inicio , controladorSesiones.validarAutenticado);
+router.get('/listar',controladorSesiones.validarAutenticado,controladorVehiculo.ListarVehiculos);
 
 
-router.post('/guardar' ,
-body('placa').isLength({min:7}).withMessage("La placa debe contener un mínimo de 7 caracteres"),
+router.post('/guardar' , controladorSesiones.validarAutenticado,
+body('placa').isLength({min:7 ,max:7}).withMessage("La placa solo debe contener 7 caracteres"),
 body('id_Modelo').isInt().withMessage("Debe enviar un ID válido"),
 body('anio').isInt().withMessage('Debe enviar un año válido'),controladorVehiculo.GuardarVehiculos);
 
 
-router.put('/modificar',
+router.put('/modificar',controladorSesiones.validarAutenticado,
 query('id_Vehiculo').isInt().withMessage("Debe enviar un ID válido"),
-body('placa').isLength({min:7}).withMessage("La placa debe contener un mínimo de 7 caracteres"),
+body('placa').isLength({min:7 ,max:7}).withMessage("La placa solo debe contener 7 caracteres"),
 body('id_Modelo').isInt().withMessage("Debe enviar un ID válido"),
 body('anio').isInt().withMessage('Debe enviar un año válido')
 ,controladorVehiculo.ModificarVehiculos);
 
 
-router.put('/eliminar',
+router.put('/eliminar',controladorSesiones.validarAutenticado,
 query('id_Vehiculo').isInt().withMessage("Debe enviar un ID válido"),controladorVehiculo.EliminarVehiculos);
 
 module.exports = router;
