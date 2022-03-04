64af7102434e8f8f384fec37130bba8033d4e711
Luis Flores <lf016158@gmail.com>
2022-03-04 10:27:54 -0600

    se arreglo las validaciones en las rutas y condiciones de express-validator

--- src/Controladores/ControladorViajes.js
+++ src/Controladores/ControladorViajes.js
@@ -1,19 +1,28 @@
 //Cargamos el modelo que creamos
 const ModeloViaje = require('../modelos/modeloViajes');
 const msj = require("../Componentes/mensaje");
-
+const con = require("../Configuracion/coneccionMysql");
+const { validationResult } = require('express-validator');
 //Definición de la función
 exports.listar = async (req, res) => {
     try {
-        // L I S T A R -- V I A J E S
-        const listaViajes = await ModeloViaje.findAll();
-        //Validación por si los campos se encuentran vacios.
-        if (listaViajes.length == 0) //Si lista viajes está vacío o nulo.
-        {
-            msj("Listar los viajes", "No existen viajes en la base de datos", 200, [], res);
-        } else { //Si lista viajes tiene datos.
-            msj("Éxito", "Lista de viajes", 200, listaViajes, res);
-        }
+        var ListaViajes = [];
+        const query = "select * from listaviajes;";
+        //Funcion para ejecutar un proceso almacenado
+        con.connect(function (err) {
+            if (err) throw err;
+            con.query(query, function (err, result, fields) {
+                if (err) throw err;
+                ListaViajes = result;
+                const totalRegistros = result.length;
+                if (!ListaViajes) {
+                    msj("Lista Vaciá", "No existen viajes en la base de datos", 200, [], res);
+                }
+                else {
+                    msj("Lista de Viajes", "Total de registros: " + totalRegistros, 200, ListaViajes, res);
+                }
+            });
+        });
     } catch (error) {
         res.status(500).json({
             error: error.toString()
@@ -21,52 +30,95 @@ exports.listar = async (req, res) => {
     }
 };
 
+function degreesToRadians(degrees) {
+    return degrees * Math.PI / 180;
+}
+
+function ObtenerTotal(distancia) {
+    var tarifaSugerida = 100;
+    var total = tarifaSugerida * distancia;
+    return parseFloat(total);
+}
+
+
+exports.getKilometros = function (lat1, lng1, lat2, lng2) {
+    // El radio del planeta tierra en metros.
+    let R = 6378137;
+    let dLat = degreesToRadians(lat2 - lat1);
+    let dLong = degreesToRadians(lng2 - lng1);
+    let a = Math.sin(dLat / 2)
+        *
+        Math.sin(dLat / 2)
+        +
+        Math.cos(degreesToRadians(lat1))
+        *
+        Math.cos(degreesToRadians(lat1))
+        *
+        Math.sin(dLong / 2)
+        *
+        Math.sin(dLong / 2);
+
+    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    let distance = R * c;
+
+    return distance.toFixed(2);
+}
+
 // G U A R D A R -- V I A J E S
 exports.guardar = async (req, res) => {
     try {
-        console.log(req.body);
-        const {
-            id_Vehiculo,
-            id_Pasajero,
-            latitud_Inicial,
-            longitud_Inicial,
-            longitud_Final,
-            latitud_Final,
-            fecha_Inicial,
-            fecha_Final,
-            fecha,
-            direccion_Inicial,
-            direccion_Final,
-            id_Tipo_Pago,
-            total,
-            id_Conductor
-        } = req.body;
-        if (!id_Vehiculo || !id_Pasajero || !latitud_Inicial || !longitud_Inicial || !longitud_Final || !latitud_Final || !fecha_Inicial || !fecha_Final ||
-            !direccion_Inicial || !direccion_Final || !id_Tipo_Pago || !total || !id_Conductor) {
-            msj("Advertencia", "Debe llenar los campos que son obligatorios", 200, [], res);
-        } else {
-            await ModeloViaje.create({
-                id_Vehiculo: id_Vehiculo,
-                id_Pasajero: id_Pasajero,
-                latitud_Inicial: latitud_Inicial,
-                longitud_Inicial: longitud_Inicial,
-                longitud_Final: longitud_Final,
-                latitud_Final: latitud_Final,
-                fecha_Inicial: fecha_Inicial,
-                fecha_Final: fecha_Final,
-                direccion_Inicial: direccion_Inicial,
-                direccion_Final: direccion_Final,
-                id_Tipo_Pago: id_Tipo_Pago,
-                total: total,
-                id_Conductor: id_Conductor
-            })
-                .then((data) => {
-                    msj("Éxito", "El registro se almacenó correctamente.", 200, data, res);
+        const validacion = validationResult(req);
+        if (!validacion.isEmpty()) {
+            msj("Datos Incorrectos", "Los datos ingresados no son validos", 200, validacion.array(), res);
+        }
+        else {
+
+
+            const {
+                id_Vehiculo,
+                id_Pasajero,
+                latitud_Inicial,
+                longitud_Inicial,
+                longitud_Final,
+                latitud_Final,
+                fecha_Inicial,
+                fecha_Final,
+                direccion_Inicial,
+                direccion_Final,
+                id_Tipo_Pago,
+                total,
+                id_Conductor
+            } = req.body;
+            if (!id_Vehiculo || !id_Pasajero || !latitud_Inicial || !longitud_Inicial || !longitud_Final || !latitud_Final || !fecha_Inicial || !fecha_Final ||
+                !direccion_Inicial || !direccion_Final || !id_Tipo_Pago || !total || !id_Conductor) {
+                msj("Advertencia", "Debe llenar los campos que son obligatorios", 200, [], res);
+            } else {
+                var distancia = this.getKilometros(parseFloat(latitud_Inicial), parseFloat(longitud_Inicial), parseFloat(latitud_Final), parseFloat(longitud_Final));
+
+                await ModeloViaje.create({
+                    id_Vehiculo: id_Vehiculo,
+                    id_Pasajero: id_Pasajero,
+                    latitud_Inicial: latitud_Inicial,
+                    longitud_Inicial: longitud_Inicial,
+                    longitud_Final: longitud_Final,
+                    latitud_Final: latitud_Final,
+                    fecha_Inicial: fecha_Inicial,
+                    fecha_Final: fecha_Final,
+                    direccion_Inicial: direccion_Inicial,
+                    direccion_Final: direccion_Final,
+                    id_Tipo_Pago: id_Tipo_Pago,
+                    total: ObtenerTotal(distancia * 0.001),
+                    distancia_Km: (distancia * 0.001),
+                    id_Conductor: id_Conductor
                 })
-                .catch((error) => {
-                    console.log(error);
-                    msj("Error", "Error al momento de guardar los datos.", 200, [], res);
-                });
+                    .then((data) => {
+                        msj("Éxito", "El registro se almacenó correctamente.", 200, data, res);
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                        msj("Error", "Error al momento de guardar los datos.", 200, [], res);
+                    });
+            }
         }
     } catch (error) {
         res.status(500).json({
@@ -78,32 +130,34 @@ exports.guardar = async (req, res) => {
 // M O D I F I C A R -- V I A J E S
 exports.modificar = async (req, res) => {
     try {
-        const {
-            id_Viaje
-        } = req.query;
-        const {
-            estado
-        } = req.body;
-        if (!estado) {
-            msj("Advertencia", "Debe ingresar un estado válido.", 200, [], res);
-        } else {
-            var buscarViaje = await ModeloViaje.findOne({
-                where: {
-                    id_Viaje: id_Viaje
-                }
-            });
-            if (!buscarViaje) {
-                msj("Advertencia", "El id del viaje no existe", 200, [], res)
+        const validacion = validationResult(req);
+        if (!validacion.isEmpty()) {
+            msj("Datos Incorrectos", "Los datos ingresados no son validos", 200, validacion.array(), res);
+        }
+        else {
+            const { id_Viaje } = req.query;
+            const { estado } = req.body;
+            if (!estado) {
+                msj("Advertencia", "Debe ingresar un estado válido.", 200, [], res);
             } else {
-                buscarViaje.estado = estado;
-                await buscarViaje.save()
-                    .then((data) => {
-                        msj("Éxito", "El registro se actualizó correctamente.", 200, data, res);
-                    })
-                    .catch((error) => {
-                        console.log(error);
-                        msj("Error", "Error al momento de actualizar los datos.", 200, [], res);
-                    });
+                var buscarViaje = await ModeloViaje.findOne({
+                    where: {
+                        id_Viaje: id_Viaje
+                    }
+                });
+                if (!buscarViaje) {
+                    msj("Advertencia", "El id del viaje no existe", 200, [], res)
+                } else {
+                    buscarViaje.estado = estado;
+                    await buscarViaje.save()
+                        .then((data) => {
+                            msj("Éxito", "El registro se actualizó correctamente.", 200, data, res);
+                        })
+                        .catch((error) => {
+                            console.log(error);
+                            msj("Error", "Error al momento de actualizar los datos.", 200, [], res);
+                        });
+                }
             }
         }
     } catch (error) {
@@ -116,29 +170,35 @@ exports.modificar = async (req, res) => {
 // E L I M I N A R -- V I A J E S
 exports.eliminar = async (req, res) => {
     try {
-        const {
-            id_Viaje
-        } = req.query; //Siempre le enviamos el ID.
-        if (!id_Viaje) {
-            msj("Advertencia", "Ingrese un viaje existente.", 200, [], res);
-        } else {
-            var buscarViaje = await ModeloViaje.findOne({
-                where: {
-                    id_Viaje: id_Viaje
-                }
-            });
-            if (!buscarViaje) {
-                msj("Advertencia", "El id del viaje no existe", 200, [], res)
+        const validacion = validationResult(req);
+        if (!validacion.isEmpty()) {
+            msj("Datos Incorrectos", "Los datos ingresados no son validos", 200, validacion.array(), res);
+        }
+        else {
+            const {
+                id_Viaje
+            } = req.query; //Siempre le enviamos el ID.
+            if (!id_Viaje) {
+                msj("Advertencia", "Ingrese un viaje existente.", 200, [], res);
             } else {
-                buscarViaje.estado = 'CNL';
-                await buscarViaje.save()
-                    .then((data) => {
-                        msj("Éxito", "El registro se elimino correctamente.", 200, data, res);
-                    })
-                    .catch((error) => {
-                        console.log(error);
-                        msj("Error", "Error al momento de eliminar los datos.", 200, [], res);
-                    });
+                var buscarViaje = await ModeloViaje.findOne({
+                    where: {
+                        id_Viaje: id_Viaje
+                    }
+                });
+                if (!buscarViaje) {
+                    msj("Advertencia", "El id del viaje no existe", 200, [], res)
+                } else {
+                    buscarViaje.estado = 'CNL';
+                    await buscarViaje.save()
+                        .then((data) => {
+                            msj("Éxito", "El registro se elimino correctamente.", 200, data, res);
+                        })
+                        .catch((error) => {
+                            console.log(error);
+                            msj("Error", "Error al momento de eliminar los datos.", 200, [], res);
+                        });
+                }
             }
         }
     } catch (error) {
